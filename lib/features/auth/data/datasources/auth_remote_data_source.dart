ea9b3bd7ab36748e0d2a866031a077815e908680
cabine13 <frbos@free.fr>
2024-11-22 19:15:09 +0100

    Data layer  (01:40:25)

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,52 @@
+import 'package:blog_app/core/errors/exception.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+// Implementation -----------------------------------------------------
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSourceImpl(this.supabaseClient);
+
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement signUpWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {
+          'name': name,
+        },
+      );
+      if (response.user == null) {
+        throw const ServerException('User is null!');
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
